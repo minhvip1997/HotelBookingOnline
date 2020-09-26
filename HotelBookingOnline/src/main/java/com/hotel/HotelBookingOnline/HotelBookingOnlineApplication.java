d76879619e22c22eadebd139291b65d0a5ca94fd
nhatminh <tnminh1997123@gmail.com>
2020-09-26 19:21:42 +0700

    commit

---
+++ HotelBookingOnline/src/main/java/com/hotel/HotelBookingOnline/HotelBookingOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.HotelBookingOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingOnlineApplication.class, args);
+	}
+
+}
